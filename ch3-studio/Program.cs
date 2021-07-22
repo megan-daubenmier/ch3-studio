01a39e7e45c80f487d6687f2a0f870e74fb3798e
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-07-21 19:23:51 -0500

    Add project files.

---
+++ ch3-studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch3_studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
